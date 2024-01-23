ad434edeb9af8b0738e43027862f5b45fe949ba5
Syed Ahtsham <syedahtshamqau@gmail.com>
2024-01-23 11:19:59 +0100

    reset to the first version

---
+++ main1.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+import pandas as pd
+
+# write a function that loads the iris dataset and plots a scatter plot of sepal length vs sepal width, iris dataset from sklearn libaray
+def load_and_plot():
+    # Load the Iris dataset from scikit-learn
+    iris = load_iris()
+    print("Shape of the data: ", iris.data.shape)
+    print("Yes")
+
+    # Create a DataFrame from the iris data
+    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    df['target'] = iris.target
+    print(df.head())
+    print(df.head())
+    # Scatter plot of sepal_length vs sepal_width
+    plt.scatter(df["sepal length (cm)"], df["sepal width (cm)"], c=df['target'], cmap='viridis', edgecolors='k')
+    plt.xlabel("Sepal Length (cm)")
+    plt.ylabel("Sepal Width (cm)")
+    plt.title("Iris Dataset: Sepal Length vs Sepal Width")
+    plt.show()
+
+#call the above function
+load_and_plot()
